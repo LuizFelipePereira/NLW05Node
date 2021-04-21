03d9b975e30cb01f26ca62c58154b02ef9bd8dfc
felipewned@hotmail.com <felipewned@hotmail.com>
2021-04-21 08:59:01 -0300

    Criacao Controller 02/05

--- src/routes.ts
+++ src/routes.ts
@@ -1,34 +1,10 @@
 import { Router } from "express";
-import { CustomRepositoryCannotInheritRepositoryError, getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controllers/SettingsController";
 
 const routes = Router();
 
-/**
- * Tipos de parametros
- * Routes Params => parametros de rotas
- * http://localhost:3333/settings/1
- * Query Params => Filtros e Buscas
- * http://localhost:333/settings/1?search=algumacoisa
- * 
- * Body params => {
- *   
- * }
- */
+const settingsController = new SettingsController();
 
-routes.post("/settings", async (request, response) => {
-     const { chat, username } = request.body;
-
-     const settingsRepository = getCustomRepository(SettingsRepository);
-     const settings = settingsRepository.create({
-          chat,
-          username
-     });
-
-     await settingsRepository.save(settings);
-
-     return response.json(settings);
-
-});
+routes.post("/settings", settingsController.create);
 
 export { routes };
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import "./database";
 import { routes} from "./routes";
 
 const app = express();
-
+app.use(express.json());
 app.use(routes);
+
+
 
 app.listen(3333, () => console.log("Server is running in port 3333"));
